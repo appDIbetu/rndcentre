549c7f1b35e8288b5e513a9883bb830afbf3b06e
appDIbetu <dipakshah401@gmail.com>
2023-05-20 14:25:42 +0530

    jsonData import

--- docs/static/js/main.a0b83190.chunk.js
+++ docs/static/js/main.a0b83190.chunk.js
@@ -135,12 +135,10 @@ var JsonData = {
     ]
 }
 
-
-
 (this["webpackJsonplandingpage-react-template"] = this["webpackJsonplandingpage-react-template"] || []).push([
     [0], {
         15: function(e) {
-            e.exports = JSON.parse(JsonData)
+            e.exports = JsonData//JSON.parse('')
         },
         23: function(e, i, t) {},
         44: function(e, i, t) {
